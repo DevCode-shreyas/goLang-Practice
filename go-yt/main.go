06988ce812850d6e9fe1f41f8dcfdb58f876c3a0
shreyas bole <shreyasbole4@gmail.com>
2024-04-06 20:31:23 +0530

    Add print statements to main.go

--- go-yt/main.go
+++ go-yt/main.go
@@ -35,4 +35,21 @@ func main() {
 
 	fmt.Println(Public)
 	fmt.Println(private)
+
+	// print
+
+	age := 22
+	name = "shreyas"
+
+	fmt.Println("Hello, ", name, "!", "You are ", age, " years old.")
+
+	// print using format specifier
+
+	fmt.Printf("Hello, %v! You are %v years old.\n", name, age)
+
+	// %v is used to print the value of the variable
+	// %T is used to print the type of the variable
+	// %f is used to print the float value - %.3f is used to print the float value upto 3 decimal places
+	// %d is used to print the integer value
+	// %s is used to print the string value
 }
